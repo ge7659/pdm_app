f25366e52fcf449bd4d9f72670b9caee1693eaa1
ge7659 <ge7659@rit.edu>
2024-10-24 10:39:31 -0400

    Created the home page. When users are logged in.

--- main.py
+++ main.py
@@ -1,4 +1,70 @@
 
+def createCollection():
+    print("Create collection")
+    homePage()
+
+def deleteCollection():
+    print("Delete collection")
+    homePage()
+
+def renameCollection():
+    print("Rename collection")
+    homePage()
+
+def listCollection():
+    print("List collection")
+    homePage()
+
+def searchVideoGame():
+    print("Search video game")
+    homePage()
+
+def followUser():
+    print("Follow user")
+    homePage()
+
+def unfollowUser():
+    print("Unfollow user")
+    homePage()
+
+def whoYouFollow():
+    print("Who you follow")
+    homePage()
+
+def logOut():
+    print("Log out")
+    mainPage()
+
+homePageChoices= {1: createCollection, 2: deleteCollection, 3: renameCollection, 4: listCollection, 5: searchVideoGame, 6: followUser, 7: unfollowUser, 8: whoYouFollow, 9: logOut}
+
+def homePage():
+    choice = 0
+
+    while choice not in homePageChoices:
+        print("Welcome to the home page")
+        print("Please choose an option by selecting a number")
+        print("Collection Operations")
+        print("1. Create Collection")
+        print("2. Delete Collection")
+        print("3. Rename Collection")
+        print("4. List Collections")
+        print("Video games operations")
+        print("5. Search for Video games")
+        print("Social Operations")
+        print("6. Follow user")
+        print("7. Unfollow user")
+        print("8. See who you follow")
+        print("9. Logout")
+        userInput = input()
+        if userInput.isdigit():
+            choice = int(userInput)
+        else:
+            print("Please enter a number.")
+            continue
+        if choice in homePageChoices:
+            homePageChoices[choice]()
+
+
 def login():
     isAuthenticated = False
     while not isAuthenticated:
@@ -9,6 +75,10 @@ def login():
         isAuthenticated = authenticate(username, password)
         if isAuthenticated == False:
             print("Invalid username or password")
+        print("Succesfully logged in")
+        print("")
+        homePage()
+
 
 def signUp():
     createdAccount = False
@@ -20,12 +90,14 @@ def signUp():
         createdAccount = createAccount(username, password)
         if createdAccount == False:
             print("Username is already taken")
-
+        print("Account Created!")
+        print("")
+        mainPage()
 def authenticate(username, password):
     return True
 
 def createAccount(username, password):
-    return False
+    return True
 
 mainPageChoices = {1: login, 2: signUp}
 
@@ -36,6 +108,7 @@ def mainPage():
         print("Please select an option.")
         print("Enter 1 to log in.")
         print("Enter 2 to sign up.")
+        print("Enter 3 to terminate Program.")
         userInput = input()
         if userInput.isdigit():
             choice = int(userInput)
@@ -44,6 +117,8 @@ def mainPage():
             continue
         if choice in mainPageChoices:
             mainPageChoices[choice]()
+        if choice == 3:
+            return
 
 
 if __name__ == '__main__':
