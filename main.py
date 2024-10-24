7718108e1484cf6b09bc251f9077e92de8189ffc
ge7659 <ge7659@rit.edu>
2024-10-24 10:05:32 -0400

    Created a basic command line login and signup page.

--- main.py
+++ main.py
@@ -1,16 +1,50 @@
-# This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+def login():
+    isAuthenticated = False
+    while not isAuthenticated:
+        print("Input a username")
+        username = input()
+        print("Input a password")
+        password = input()
+        isAuthenticated = authenticate(username, password)
+        if isAuthenticated == False:
+            print("Invalid username or password")
+
+def signUp():
+    createdAccount = False
+    while not createdAccount:
+        print("Input a username")
+        username = input()
+        print("Input a password")
+        password = input()
+        createdAccount = createAccount(username, password)
+        if createdAccount == False:
+            print("Username is already taken")
+
+def authenticate(username, password):
+    return True
+
+def createAccount(username, password):
+    return False
+
+mainPageChoices = {1: login, 2: signUp}
+
+def mainPage():
+    choice = 0
+
+    while choice not in mainPageChoices:
+        print("Please select an option.")
+        print("Enter 1 to log in.")
+        print("Enter 2 to sign up.")
+        userInput = input()
+        if userInput.isdigit():
+            choice = int(userInput)
+        else:
+            print("Please enter a number.")
+            continue
+        if choice in mainPageChoices:
+            mainPageChoices[choice]()
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    mainPage()
